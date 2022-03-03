8dd6d6f1bb151e9d823454ec121dbf4abdb651f7
Jozua Zwijnenburg <jozua@null.net>
2022-03-04 09:53:47 +1030

    Journal updates implemented

--- routes/journal.js
+++ routes/journal.js
@@ -9,11 +9,10 @@ router.get("/get", function(req, res, next) {
             res.sendStatus(500);
             return;
         }
-        var query = "SELECT j_id, title, description, timestamp, style FROM Journals INNER JOIN Icons WHERE c_id = ?;";
+        var query = "SELECT j_id, title, description, timestamp, style FROM Journals INNER JOIN Icons ON Journals.i_id = Icons.i_id WHERE c_id = ?;";
         connection.query(query, [req.cookies.c_id], function(err, rows, fields) {
             connection.release();
             if (err) {
-            	console.log(err)
                 res.sendStatus(500);
                 return;
             }
@@ -42,7 +41,6 @@ router.get("/new", function(req, res, next) {
         var query = "INSERT INTO Journals (c_id, title, description, timestamp, i_id) VALUES (?, 'Title', 'Description', CURRENT_TIMESTAMP(), 1);";
         connection.query(query, [req.cookies.c_id], function(err, rows, fields) {
             if (err) {
-            	console.log(err)
                 res.sendStatus(500);
                 return;
             }
@@ -50,7 +48,6 @@ router.get("/new", function(req, res, next) {
 			connection.query(query, [req.cookies.c_id], function(err, rows, fields) {
 			    connection.release();
 			    if (err) {
-			    	console.log(err)
 			        res.sendStatus(500);
 			        return;
 			    }
@@ -64,7 +61,6 @@ router.post("/save", function(req, res, next) {
     req.pool.getConnection(function(err, connection)
     {
         if (err) {
-        	console.log("one");
             res.sendStatus(500);
             return;
         }
@@ -73,23 +69,17 @@ router.post("/save", function(req, res, next) {
         var query = "SELECT i_id FROM Icons WHERE style = ?;";
         connection.query(query, [req.body.style], function(err, firstrows, fields) {
             if (err){
-            	console.log("two");
                 res.sendStatus(500);
                 return;
             }
-            console.log("i_id for " + req.body.style + " is " + firstrows[0]["i_id"]);
             // Update the journal
 		    var query = "UPDATE Journals SET title = ?, description = ?, i_id = ? WHERE j_id = ?;";
 		    connection.query(query, [req.body.title, req.body.description, firstrows[0]["i_id"], req.body.j_id], function(err, secondrows, fields) {
 		    	connection.release();
 		        if (err){
-		        	console.log("three");
-		        	console.log(err);
 		            res.sendStatus(500);
 		            return;
 		        }
-		        console.log("The update statement is ");
-		        console.log(secondrows);
 		        res.sendStatus(200);
 		    });
         });
@@ -107,7 +97,6 @@ router.get("/fetch/:j_id", function(req, res, next) {
         connection.query(query, [req.params.j_id], function(err, rows, fields) {
             connection.release();
             if (err) {
-            	console.log(err)
                 res.sendStatus(500);
                 return;
             }
@@ -128,7 +117,6 @@ router.get("/icons", function(req, res, next) {
         connection.query(query, [req.params.j_id], function(err, rows, fields) {
             connection.release();
             if (err) {
-            	console.log(err)
                 res.sendStatus(500);
                 return;
             }
